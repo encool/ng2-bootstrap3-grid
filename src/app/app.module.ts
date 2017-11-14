ed7717e3419d431bacfc90e486e1ab6b4f9211a0
qiao.li <qiao.li@chinacreator.com>
2017-11-14 23:14:32 +0800

    ng5 init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Bootstrap3GridModule } from "../../grid";
+import { Bootstrap3GridModule } from "ng2-bootstrap3-grid";
 
 @NgModule({
   declarations: [
